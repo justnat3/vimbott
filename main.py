60ab1ea1d9599cb78d40febb08183ed19ed492bd
justnat3 <nd2010@reedusa.com>
2020-12-12 15:08:04 -0600

    added env, added extra functionality

--- main.py
+++ main.py
@@ -1,24 +1,66 @@
 from twitchio.ext import commands
+import random
+import os
 
 
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(
-            irc_token="oauth:gqtncaviovcz1qumzy1qmozinqlh7y",
-            client_id="wersfmv0rihk1rq4hondy6yygudjvg",
+            irc_token=os.getenv("irc_token"),
+            client_id=os.getenv("client_id"),
             nick="vimbott",
             prefix="!",
             initial_channels=["#doctorvim"],
         )
 
-    # Events don't need decorators when subclassed
-    async def event_ready(self): print(f"Ready | {self.nick}")
+    async def event_ready(self):
+        print(f"Ready | {self.nick}")
 
-    async def event_message(self, message): await self.handle_commands(message)
+    async def event_message(self, message):
+        await self.handle_commands(message)
 
-    # Commands use a decorator...
     @commands.command(name="dot")
-    async def my_command(self, ctx): await ctx.send(f"https://github.com/justnat3")
+    async def dot(self, ctx):
+        await ctx.send("")
+
+    @commands.command(name="cproject")
+    async def cproject(self, ctx):
+        try:
+            if ctx.author.is_mod:
+                text = str(ctx.message.content)
+                if len(text) > 1:
+                    with open("./project.txt", "w") as f:
+                        a = text.strip("!cproject")
+                        f.write(a)
+                    if len(text) < 1:
+                        await ctx.send("Changed")
+                    f.close()
+                else:
+                    await ctx.send("Project was unchanged or empty")
+            else:
+                await ctx.send(f"You are not able to do this {ctx.author.name}")
+        except Exception as err:
+            print(err)
+
+    @commands.command(name="project")
+    async def project(self, ctx):
+        with open("./project.txt", "r") as f:
+            await ctx.send(str(f.read()))
+        f.close()
+
+    @commands.command(name="roll")
+    async def roll(self, ctx):
+        r = random.randint(0, 20)
+        await ctx.send(r)
+
+    @commands.command(name="dan")
+    async def dan(self, ctx):
+        await ctx.send("Dan Likes Men")
+
+    @commands.command(name="github")
+    async def git(self, ctx):
+        await ctx.send("https://github.com/justnat3")
+
 
 if __name__ == "__main__":
     bot = Bot()
